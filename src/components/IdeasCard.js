cb71d672911523295fcbc9d0fed17c4328fd6610
MatteoMatteoMatteo <S1910629019@students.fh-hagenberg.at>
2022-04-25 23:19:16 +0200

    mp3s and sooo many songs

--- src/components/IdeasCard.js
+++ src/components/IdeasCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './IdeasCard.css';
 
-export default function IdeasCard({ header, song, bgColor }) {
+export default function IdeasCard({ header, song, bgColor, volume }) {
   return (
     <div style={{ backgroundColor: bgColor }} className='cardContainerIdeas'>
-      {header}
-      <audio controls autoplay src={song}></audio>
+      <span style={{ marginBottom: '8px' }}> {header}</span>
+
+      <audio controls volume={volume} controlsList={'nodownload'} src={song}></audio>
     </div>
   );
 }
