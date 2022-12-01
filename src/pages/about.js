1e86a4804ea60ef9ffc6ca48e5938133be0c82bc
MatteoMatteoMatteo <m.prock@outlook.com>
2022-12-01 17:58:58 +0100

    typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,8 +17,8 @@ export default function about() {
           <div className='contactHolder'>
             {/* <img className={'meImage'} src={Me} alt='me'></img> */}
             <h3 style={{ fontSize: '1.4rem' }} className='aboutHolder'>
-              After my studies in Innsbruck at the Medienkolleg and the MCI, I'm recently finished
-              my Master's Degree in Interactive Media at the University of Applied Sciences Upper
+              After my studies in Innsbruck at the Medienkolleg and the MCI, I recently finished my
+              Master's Degree in Interactive Media at the University of Applied Sciences Upper
               Austria.
             </h3>
             <h3 style={{ fontSize: '1.4rem' }} className='aboutHolder'>
